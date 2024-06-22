d35148fef981e1a8ccea4ecd086cd8e79d5bdcef
dafreshy <141530122+dafreshy@users.noreply.github.com>
2024-06-22 16:35:53 -0400

    Update login.js

--- login.js
+++ login.js
@@ -1,7 +1,7 @@
 document.getElementById('login-form').addEventListener('submit', function(event) {
     event.preventDefault();
     const password = document.getElementById('password').value;
-    if (password === 'yourpassword') {
+    if (password === 'snickers') {
         window.location.href = 'index.html';
     } else {
         document.getElementById('error-message').style.display = 'block';
