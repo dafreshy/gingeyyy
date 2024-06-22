14ef812ec79941bead14329d33ad8691de94c3c9
dafreshy <141530122+dafreshy@users.noreply.github.com>
2024-06-22 16:45:35 -0400

    Update login.js

--- login.js
+++ login.js
@@ -1,7 +1,7 @@
 document.getElementById('password-form').addEventListener('submit', function(event) {
     event.preventDefault();
     const password = document.getElementById('password').value;
-    if (password === 'yourpassword') {
+    if (password === 'snickers') {
         document.getElementById('passwordModal').style.display = 'none';
         document.getElementById('mainContent').style.display = 'block';
     } else {
