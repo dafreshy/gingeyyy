88206f7c6170aa900e14e6bdac0b9900b1603812
dafreshy <141530122+dafreshy@users.noreply.github.com>
2024-06-22 17:31:09 -0400

    Update login.js

--- login.js
+++ login.js
@@ -1,9 +1,1 @@
-document.getElementById('login-form').addEventListener('submit', function(event) {
-    event.preventDefault();
-    const password = document.getElementById('password').value;
-    if (password === '1205') {
-        window.location.href = 'index.html';
-    } else {
-        document.getElementById('error-message').style.display = 'block';
-    }
-});
+//NO NEED FOR FILE
