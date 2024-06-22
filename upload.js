76248b5ad3d83519156dd227409e04cdebb3a33f
dafreshy <141530122+dafreshy@users.noreply.github.com>
2024-06-22 17:53:56 -0400

    Update upload.js

--- upload.js
+++ upload.js
@@ -2,28 +2,21 @@ document.getElementById('uploadForm').addEventListener('submit', function(event)
     event.preventDefault();
 
     const formData = new FormData(this);
-    const fileInput = document.getElementById('fileInput');
     const datetimeInput = document.getElementById('datetimeInput').value;
 
-    if (fileInput.files.length > 0) {
-        const file = fileInput.files[0];
-        formData.append('file', file);
-        formData.append('datetime', datetimeInput);
+    formData.append('datetime', datetimeInput);
 
-        fetch('upload.php', {
-            method: 'POST',
-            body: formData
-        })
-        .then(response => response.json())
-        .then(data => {
-            alert(data.message);
-            location.reload(); // Reload the page to show uploaded image
-        })
-        .catch(error => {
-            console.error('Error:', error);
-            alert('Error uploading image.');
-        });
-    } else {
-        alert('Please select an image file.');
-    }
+    fetch('upload.php', {
+        method: 'POST',
+        body: formData
+    })
+    .then(response => response.json())
+    .then(data => {
+        alert(data.message);
+        location.reload(); // Reload the page to show uploaded image
+    })
+    .catch(error => {
+        console.error('Error:', error);
+        alert('Error uploading image.');
+    });
 });
