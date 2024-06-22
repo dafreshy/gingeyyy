72e55c672ae1ba3ebde9653d58821974e5c60fc8
dafreshy <141530122+dafreshy@users.noreply.github.com>
2024-06-22 17:08:05 -0400

    Update login.js

--- login.js
+++ login.js
@@ -1,19 +1,9 @@
-window.onload = function() {
-    const modal = document.getElementById('passwordModal');
-    const form = document.getElementById('password-form');
-    const passwordInput = document.getElementById('password');
-    const errorMessage = document.getElementById('error-message');
-
-    modal.style.display = 'block'; // Show the modal on page load
-
-    form.addEventListener('submit', function(event) {
-        event.preventDefault();
-        const password = passwordInput.value;
-        if (password === '1205') { // Replace 'yourpassword' with your actual password
-            modal.style.display = 'none'; // Hide the modal
-            displayImage(); // Call displayImage after password is validated
-        } else {
-            errorMessage.style.display = 'block';
-        }
-    });
-};
+document.getElementById('login-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    const password = document.getElementById('password').value;
+    if (password === '1205') {
+        window.location.href = 'index.html';
+    } else {
+        document.getElementById('error-message').style.display = 'block';
+    }
+});
